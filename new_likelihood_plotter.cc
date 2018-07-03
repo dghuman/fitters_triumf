9aeefa4834d16f5365395191f1527fafbb47f538
Dilraj Ghuman (UWaterloo student) <dghuman@triumf.ca>
2018-07-03 16:18:53 -0700

    Updated the likelihood plotter so that the bins are centered around 0

--- new_likelihood_plotter.cc
+++ new_likelihood_plotter.cc
@@ -8,34 +8,33 @@ Requires that we know the precision for the spatial variables and the time varia
 {
 #include <vector>     
 
-  const int ntuplecount = 1;
+  const int ntuplecount = 2;
 
   TObjArray xtemphists,ytemphists,ztemphists,ttemphists;    // Histogram array that will hold the hists that will be filled when each loglikelihood is distributed.
   
-  /*TH1F* xhist;
-  TH1F* yhist;
-  TH1F* zhist;
-  TH1F* thist;*/
-
   TH1F* xhist_final;
   TH1F* yhist_final;
   TH1F* zhist_final;
   TH1F* thist_final;
 
-  Int_t check,count,mincount,spatial_steps,time_steps,count2,x_index,y_index,z_index,t_index,ntuple_entries;
-  Float_t xpos,ypos,zpos,time,loglikelihood;
+  TF1* time_norm;
+  TF1* spatial_norm;
+
+  Int_t check,count,mincount,spatial_steps,time_steps,count2,x_index,y_index,z_index,t_index,ntuple_entries,gmin_xpos,gmin_ypos,gmin_zpos,gmin_time;
+  Float_t xpos,ypos,zpos,time,loglikelihood,gmin_loglikelihood;
 
   // Read in the file and make an output file
-  TFile TheFile("./fiTQun_015ns_delay.root");
-  TFile NewFile("./Likelihood_Plots.root","RECREATE");
+  TFile TheFile("./fiTQun_single_events.root");
+  TFile NewFile("./TESTING.root","RECREATE");
 
-  // The Ntuple to be read from
+  // The Ntuples to be read from and written to
   TNtuple* Ntuple; 
+  TNtuple* EventGlobalMin = new TNtuple("Global_Min", "Global Minimum","event:xpos:ypos:zpos:time:loglikelihood");
 
   // Values set apriori according to the fiTQun fitting parameters.
   const Int_t inf = 1000000000;
   const Int_t time_precision = 1;
-  const Int_t time_min = 840;
+  const Int_t time_min = 950;
   const Int_t time_max = 1000;
   const Int_t spatial_min = -500;
   const Int_t spatial_max = 500;
@@ -52,32 +51,38 @@ Requires that we know the precision for the spatial variables and the time varia
   } else { 
     mincount = spatial_steps;
     count2 = time_steps;
-    cout << "More spatial steps than time steps!" << endl;
+    cout << "More spatial steps than time steps! Stopping here." << endl;
+    break;
   }
 
   check = 0;
   count = 0;
   // Allocate the matrices to store the data
-  vector<Float_t> time_mat(time_steps);
-  vector<Float_t> xpos_mat(spatial_steps);
-  vector<Float_t> ypos_mat(spatial_steps);
-  vector<Float_t> zpos_mat(spatial_steps);
-  // Fill the vectors with infs so that we can 
-  for (int m = 0; m < time_steps; m++) {
-    switch(check) {
-    case 0: 
-      xpos_mat[m] = inf;
-      ypos_mat[m] = inf;
-      zpos_mat[m] = inf;
-    case 1:
-      time_mat[m] = inf;
-      break;
-    }
-    if (m == spatial_steps - 1) { check = 1; }
-  }
+  vector<Float_t> time_mat;
+  vector<Float_t> xpos_mat;
+  vector<Float_t> ypos_mat;
+  vector<Float_t> zpos_mat;
 
   // Loop through the ntuples
-  for (int i = 0; i < ntuplecount; i++) {
+  for (int i = 1; i < ntuplecount; i++) {
+    // Clear the vectors of the previous ntuple values
+    time_mat.clear();
+    xpos_mat.clear();
+    ypos_mat.clear();
+    zpos_mat.clear();
+    // refill the vectors with infs so that we can compare values
+    for (int m = 0; m < time_steps; m++) {
+      switch(check) {
+      case 0: 
+	xpos_mat.push_back(inf);
+	ypos_mat.push_back(inf);
+	zpos_mat.push_back(inf);
+      case 1:
+	time_mat.push_back(inf);
+	break;
+      }
+      if (m == spatial_steps - 1) { check = 1; }
+    }
     TheFile->cd();
     cout << "Working on Ntuple number " << i << endl;
     Ntuple = (TNtuple*)TheFile.Get(Form("likelihoodprefit_Event%03d",i));    // Read from the next ntuple
@@ -86,16 +91,26 @@ Requires that we know the precision for the spatial variables and the time varia
     Ntuple->SetBranchAddress("zpos",&zpos);
     Ntuple->SetBranchAddress("time",&time);
     Ntuple->SetBranchAddress("loglikelihood",&loglikelihood);
-    xhist_final = new TH1F(Form("loglikelihood_vs_xpos_Event%03d",i), "Loglikelihood vs xpos", spatial_steps, spatial_min, spatial_max);
-    yhist_final = new TH1F(Form("loglikelihood_vs_ypos_Event%03d",i), "Loglikelihood vs ypos", spatial_steps, spatial_min, spatial_max);
-    zhist_final = new TH1F(Form("loglikelihood_vs_zpos_Event%03d",i), "Loglikelihood vs zpos", spatial_steps, spatial_min, spatial_max);
-    thist_final = new TH1F(Form("loglikelihood_vs_time_Event%03d",i), "Loglikelihood vs time", time_steps, time_min, time_max);
+    xhist_final = new TH1F(Form("loglikelihood_vs_xpos_Event%03d",i), "Loglikelihood vs xpos", spatial_steps+1, (float)spatial_min - ((float)spatial_precision)/2.0, (float)spatial_max + ((float)spatial_precision)/2.0);
+    yhist_final = new TH1F(Form("loglikelihood_vs_ypos_Event%03d",i), "Loglikelihood vs ypos", spatial_steps+1, (float)spatial_min - ((float)spatial_precision)/2.0, (float)spatial_max + ((float)spatial_precision)/2.0);
+    zhist_final = new TH1F(Form("loglikelihood_vs_zpos_Event%03d",i), "Loglikelihood vs zpos", spatial_steps+1, (float)spatial_min - ((float)spatial_precision)/2.0, (float)spatial_max + ((float)spatial_precision)/2.0);
+    thist_final = new TH1F(Form("loglikelihood_vs_time_Event%03d",i), "Loglikelihood vs time", time_steps+1, (float)time_min - ((float)time_precision)/2.0, (float)time_max + ((float)spatial_precision)/2.0);
     ntuple_entries = Ntuple->GetEntries();
     check = 0;
+    gmin_loglikelihood = inf;
     for (int k = 0; k < ntuple_entries; k++) {
       Ntuple->GetEntry(k);
-      if (loglikelihood != loglikelihood || loglikelihood > inf) {loglikelihood = inf;} // Set large/nan loglikelihoods to inf
+      if (loglikelihood != loglikelihood) {loglikelihood = inf;} // Set nan loglikelihoods to inf
       //cout << "Loglikelihood at " << k << " is " << loglikelihood << endl; // Debugging
+      // Keep track of the minimum loglikelihood observed
+      if (loglikelihood < gmin_loglikelihood) {
+	cout << "New minimum Loglikelihood is " << loglikelihood << endl;
+	gmin_loglikelihood = loglikelihood;
+	gmin_xpos = xpos;
+	gmin_ypos = ypos;
+	gmin_zpos = zpos;
+	gmin_time = time;
+      }
       // Fill the appropriate vectors with the loglikelihood if it is smaller than the prior one
       x_index = ((int)xpos - spatial_min)/spatial_precision;
       y_index = ((int)ypos - spatial_min)/spatial_precision;
@@ -114,7 +129,7 @@ Requires that we know the precision for the spatial variables and the time varia
 	time_mat[t_index] = loglikelihood;
       }
     }
-    // Write the minimum of each column vector to the appropriate 
+    // Write the minimum of each column vector to the appropriate histogram bin
     check = 0;
     for (int j = 0; j < time_steps; j++) {
       switch(check) {
@@ -129,13 +144,24 @@ Requires that we know the precision for the spatial variables and the time varia
       if (j == spatial_steps - 1) { check = 1; }
     }
     NewFile.cd();
-    cout << "Saving plots ... " << endl;
+    cout << "Filling ntuple with global minimum position ..." << endl;
+    // Save to the ntuple
+    EventGlobalMin->Fill((float)i,(float)gmin_xpos,(float)gmin_ypos,(float)gmin_zpos,(float)gmin_time,(float)gmin_loglikelihood);
+    // Normalize the plots that were made to the global minimum experienced during the process
+    time_norm = new TF1(Form("time_norm%03d",i),"-1",time_min,time_max);
+    spatial_norm = new TF1(Form("spatial_norm%03d",i),"-1",spatial_min,spatial_max);
+    xhist_final->Add(spatial_norm,gmin_loglikelihood);
+    yhist_final->Add(spatial_norm,gmin_loglikelihood);
+    zhist_final->Add(spatial_norm,gmin_loglikelihood);
+    thist_final->Add(time_norm,gmin_loglikelihood);    
     // Save the plots that we have made
+    cout << "Saving plots ... " << endl;
     NewFile.cd();
     xhist_final->Write();
     yhist_final->Write();
     zhist_final->Write();
     thist_final->Write();
   }
+  EventGlobalMin->Write();
 }
 
